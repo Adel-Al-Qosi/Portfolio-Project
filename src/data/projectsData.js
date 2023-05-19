9ea9bc6032d2bca472bf403da48553914692ea94
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-05-19 16:11:01 +0300

    add new project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,22 +1,27 @@
 import spaceTourismImage from "../images/projects-images/Screenshot from 2023-05-04 19-27-52.png";
-import frontEnd1 from "../images/projects-images/front-end-1.png"
-
+import frontEnd1 from "../images/projects-images/front-end-1.png";
+import dashboard from "../images/projects-images/dashboard.png";
 const projectsData = [
   {
     name: "Front-end E-commerce project",
     description: "a cool Front-end site built with react and css",
     image: frontEnd1,
-    link: "https://adel-front-end-1.netlify.app/"
+    link: "https://adel-front-end-1.netlify.app/",
+  },
+
+  {
+    name: "Time tracking dashboard",
+    description: "built with react.js, and css.",
+    image: dashboard,
+    link: "https://adel-time-tracker-dashboard.netlify.app/",
   },
 
   {
     name: "Space Tourism project",
     description: "still working on this project, to improve my css skills",
     image: spaceTourismImage,
-    link: "https://github.com/Adel-Al-Qosi/space-tourism"
-  }
-
-  
+    link: "https://github.com/Adel-Al-Qosi/space-tourism",
+  },
 ];
 
 export { projectsData };
